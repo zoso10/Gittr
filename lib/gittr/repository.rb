3e8a9267bcee48168cdfca75fe036e20bd7fd652
Tyler Ewing <tewing10@gmail.com>
2015-01-19 21:30:51 -0500

    Refactor Repository so room maps to Room class

--- lib/gittr/repository.rb
+++ lib/gittr/repository.rb
@@ -2,6 +2,10 @@ module Gittr
   class Repository
     include HashConstructor
 
-    attr_accessor :id, :name, :description, :uri, :private, :exists, :avatar_url
+    attr_accessor :id, :name, :description, :uri, :private, :exists, :avatar_url, :room
+
+    def room=(room)
+      @room = Room.new(room)
+    end
   end
 end
--- spec/repository_spec.rb
+++ spec/repository_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe Gittr::Repository do
 
-  subject(:repository){Gittr::Repository.new({"id"=>7411076, "name"=>"thoughtbot/hound", "description"=>"Review your Ruby code for style guide violations with a trusty hound.", "uri"=>"thoughtbot/hound", "private"=>false, "exists"=>false, "avatar_url"=>"https://avatars.githubusercontent.com/u/6183?v=3"})}
-
+  subject(:repository){Gittr::Repository.new({"id"=>29428880, "name"=>"zoso10/Gittr", "description"=>"Gitter REST API wrapper", "uri"=>"zoso10/Gittr", "private"=>false, "room"=> {"id"=>"54bc01e3db8155e6700ecbca", "name"=>"zoso10/Gittr", "topic"=>"Gitter REST API wrapper", "uri"=>"zoso10/Gittr", "oneToOne"=>false, "userCount"=>1, "unreadItems"=>0, "mentions"=>0, "lurk"=>false, "url"=>"/zoso10/Gittr", "githubType"=>"REPO", "security"=>"PUBLIC", "premium"=>false, "noindex"=>false}, "exists"=>true, "avatar_url"=>"https://avatars.githubusercontent.com/u/1211946?v=3"})} 
   it 'should instantiate from hash' do
     expect(repository).to_not be_nil
-    expect(repository.name).to eq('thoughtbot/hound')
-    expect(repository.uri).to eq('thoughtbot/hound')
+    expect(repository.name).to eq('zoso10/Gittr')
+    expect(repository.uri).to eq('zoso10/Gittr')
     expect(repository.private).to eq(false)
+    expect(repository.room.name).to eq('zoso10/Gittr')
   end
 end
